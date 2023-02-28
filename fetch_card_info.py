b5a27dc0e8793cac6cd85b7ede9438ba6b7c2b8e
Ganesh Kakade <17514413+ganeshkakade@users.noreply.github.com>
2023-02-28 20:53:11 +0530

    fixed api response size issue

--- fetch_card_info.py
+++ fetch_card_info.py
@@ -1,22 +1,20 @@
 import requests
-import json
+from json import dumps
 
-# note: card image can be fetched with link using id as https://s3.duellinksmeta.com/cards/{id}_w140.webp
+# note: card images can be accessed from link using id i.e https://s3.duellinksmeta.com/cards/{id}_w140.webp
 
-def card_info(result=[], page=0):
-    response = requests.get(f"https://www.masterduelmeta.com/api/v1/cards?page={page}&limit=3000")
+def card_info(result=[], page=1):
+    response = requests.get(f"https://www.masterduelmeta.com/api/v1/cards?cardSort=popRank&aggregate=search&page={page}&limit=2500")
     response.raise_for_status()
     responseJSON = response.json()
 
     if(not responseJSON):
-        return json.dumps(result)
-    else:
-        page = page + 1
-        result = result + responseJSON
-        card_info(result, page)
-    return json.dumps(result)       
+        return result
+    
+    page = page + 1
+    result = result + responseJSON
+    return card_info(result, page)
 
 if __name__ == '__main__':
-    jsonFile = open("card_info_data.json", "w")
-    jsonFile.write(card_info())
-    jsonFile.close()
+    with open("card_info_data.json", "w") as json_file:
+        json_file.write(dumps(card_info()))
